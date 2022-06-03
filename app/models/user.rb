fb984d3a79505081872401d5abe640fb6b7372be
Robbie Prokop <robbieprokop@gmail.com>
2022-06-03 03:26:59 +0000

    dynamically change stripe payment info to match user info

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
+
 end
 
 
