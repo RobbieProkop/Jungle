896e8a6a3f050cf00360a5c5f8e79d9511cb6419
Robbie Prokop <robbieprokop@gmail.com>
2022-06-07 16:00:20 +0000

    write tests for JUNGLE using cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,11 @@
-describe("visit home page", () => {
+describe("Jungle TESTS!", () => {
   it("visits the home page", () => {
     cy.visit("0.0.0.0:3000");
+  });
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
   });
 });
